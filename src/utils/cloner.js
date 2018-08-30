321035633574cad836c7d1ec9276cd9c0678b50f
Drew Showalter <maelstroswim@gmail.com>
2018-08-30 20:30:34 +1000

    :sparkles: add util for cloning a type of project.

---
+++ src/utils/cloner.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+/**
+ * Recursive cloner for our project,
+ * calls itself to construct a new project
+ *
+ * @param {String} newPathSection - project folder that mirrors the following local path
+ * @param {String} localSection - local filepath being read and cloned
+ * @param {String} replaceName - function to replace the `new-{TYPE}` placeholder
+ */
+const recursiveClone = (newPathSection, localSection, replaceName) => {
+  const stats = fs.statSync(localSection);
+
+  if (stats.isDirectory()) {
+    try {
+      fs.mkdirSync(newPathSection);
+    } catch (error) {
+      runningLog.fatal(`Unable to create new directory ${newPathSection}`);
+      throw error;
+    }
+
+    const filePaths = fs.readdirSync(localSection);
+    return filePaths.map(path =>
+      recursiveClone(
+        `${newPathSection}/${path}`,
+        `${localSection}/${path}`,
+        replaceName
+      )
+    );
+  }
+
+  const contents = fs.readFileSync(localSection, { encoding: 'utf8' });
+  return fs.writeFileSync(newPathSection, replaceName(contents));
+};
+
+/**
+ * Tries to clone a project into a new directory, changing `new-{TYPE}` into
+ * the new project name. Instantiates `recursiveClone`
+ *
+ * @param {String} [projectName] - new folder to create the project in
+ * @param {Object} options - optional file io properties
+ * @param {String} options.name - alternate internal name for the project
+ */
+module.exports.default = (type, projectName, options = {}) => {
+  if (type !== 'cli') {
+    runningLog.fatal('No other project types exist at the moment');
+    throw new Error();
+  }
+
+  const { prettyName } = options;
+  const name = prettyName || projectName;
+
+  const RE_NAME_REPLACE = new RegExp(`new-${type}`, 'g');
+  const replaceName = contents => contents.replace(RE_NAME_REPLACE, name);
+
+  const newProjectDir = `./${projectName}`;
+
+  recursiveClone(newProjectDir, `${__dirname}/../cli`, replaceName);
+};
---
+++ test/utils/cloner_test.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import mockFs from 'mock-fs';
+import '../_helper';
+
+const { default: cloneIt } = require('../../src/utils/cloner');
+
+test.afterEach.always(mockFs.restore);
+
+test('clones our example directory to a new location', t => {
+  const mockCliDir = `${__dirname.replace(/\/test\//g, '/src/')}/../cli`;
+  mockFs({
+    [mockCliDir]: {
+      'index.js': 'our cool util, not at all new-cli',
+    },
+  });
+
+  cloneIt('cli', 'baller-project', {});
+
+  t.deepEqual(fs.readdirSync('./baller-project'), ['index.js']);
+  t.deepEqual(
+    fs.readFileSync('./baller-project/index.js', { encoding: 'utf8' }),
+    'our cool util, not at all baller-project'
+  );
+});
+
+test('allows the user to specify a pretty internal name', t => {
+  const mockCliDir = `${__dirname.replace(/\/test\//g, '/src/')}/../cli`;
+  mockFs({
+    [mockCliDir]: {
+      'index.js': 'our cool util, not at all new-cli',
+    },
+  });
+
+  cloneIt('cli', 'baller-project', { prettyName: 'woooooo-js' });
+
+  t.deepEqual(fs.readdirSync('./baller-project'), ['index.js']);
+  t.deepEqual(
+    fs.readFileSync('./baller-project/index.js', { encoding: 'utf8' }),
+    'our cool util, not at all woooooo-js'
+  );
+});
