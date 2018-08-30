adda754edbb50e6dfb7092c960cc8f5026f925e6
Drew Showalter <maelstroswim@gmail.com>
2018-08-30 20:32:04 +1000

    :sparkles: add cli creation subcommand.

---
+++ src/cli.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const _ = require('lodash');
+const program = require('commander');
+const { Signale } = require('signale');
+const { default: cloner } = require('./utils/cloner');
+
+const runningLog = new Signale({ interactive: true, scope: 'new:cli' });
+runningLog.start('Beginning process');
+
+global._ = _;
+global.runningLog = runningLog;
+
+program
+  .arguments('<dir>')
+  .option(
+    '-n, --pretty-name [string]',
+    'optional internally applied application name'
+  )
+  .action((newDir, options) => {
+    runningLog.info('Cloning project');
+
+    cloner('cli', newDir, options);
+
+    runningLog.success('Cloned new cli!');
+  });
+
+program.parse(process.argv);
